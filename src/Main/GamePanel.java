1dbc4ff073ae87bac64c24163f695379b7e04d97
Riley <seanrileydelacruz782@gmail.com>
2026-03-07 17:47:56 +0800

    Added Animation sprites for one of our characters, Revamped how movement is implemented

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -10,29 +10,62 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static utils.constants.PlayerConstants.*;
+import static utils.constants.Directions.*;
+
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private int moveX = 1, moveY = 1;
-    private BufferedImage img, subImg;
+    private BufferedImage img;
+    private BufferedImage[][] sylvaraAnimation;
+    private int animationTick, animationIndex, animationSpeed = 15;
+    private int playerAction = IDLE;
+    private int playerDirection = -1;
+    private boolean moving = false;
+
+
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
         importImg();
+        loadAnimations();
+
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+
+        setFocusable(true);
+    }
+
+    public void loadAnimations()
+    {
+        sylvaraAnimation = new BufferedImage[3][9];
+
+        for(int i = 0; i < sylvaraAnimation.length; i++)
+        {
+            for(int j = 0; j < sylvaraAnimation[i].length; j++)
+            {
+               sylvaraAnimation[i][j] = img.getSubimage(j * 32, i * 32, 32, 32 );
+            }
+        }
     }
 
     private void importImg() {
-        String path = "/Characters/Hero/Soldier-Walk.png";
+        String path = "/Characters/Sylvara/SylvaraSpriteSheet.png";
         InputStream is = getClass().getResourceAsStream(path);
 
         try {
             img = ImageIO.read(is);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -41,21 +74,75 @@ public class GamePanel extends JPanel {
         setPreferredSize(size);
     }
 
-    public void changeX(int value) {
-        this.moveX += value;
-    }
-    public void changeY(int value) {
-        this.moveY += value;
+    public void setDirection(int direction)
+    {
+        this.playerDirection = direction;
+        moving = true;
     }
 
-    public void setPosition(int x, int y) {
-        this.moveX = x;
-        this.moveY = y;
+    public void setMoving(boolean moving)
+    {
+        this.moving = moving;
+    }
+
+
+
+    public void updateAnimationTick()
+    {
+        animationTick++;
+        if(animationTick >= animationSpeed)
+        {
+            animationTick = 0;
+            animationIndex++;
+            if(animationIndex >= GetSpriteAmount(playerAction))
+            {
+                animationIndex = 0;
+            }
+        }
+    }
+
+    private void setAnimation()
+    {
+        if(moving)
+        {
+            if(playerDirection == RIGHT) playerAction = WALKR;
+
+            if(playerDirection == LEFT) playerAction = WALKL;
+        }
+        else playerAction = IDLE;
+    }
+
+    private void updatePos()
+    {
+        if(moving)
+        {
+            switch (playerDirection)
+            {
+                case LEFT:
+                    moveX -= 5;
+                    break;
+                case UP:
+                    moveY -= 5;
+                    break;
+                case RIGHT:
+                    moveX += 5;
+                    break;
+                case DOWN:
+                    moveY +=5;
+                    break;
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        subImg = img.getSubimage(1, 1, 64, 64);
-        g.drawImage(subImg, moveX, moveY, 130, 150, null);
+        updateAnimationTick();
+
+        setAnimation();
+        updatePos();
+
+        g.drawImage(sylvaraAnimation[playerAction][animationIndex], (int)moveX, (int)moveY, 250, 250, null);
     }
+
+
 }
